6c88644a7adb6b747489240f1e9caf26e83d9326
adinda0112 <dinda0112112@gmail.om>
2024-01-15 12:51:39 +0700

    Initial commit

---
+++ src/UAS1A.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+public class UAS1A {
+    public static void main(String[] args) throws IOException {
+        ArrayList<Integer> g = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Input jumlah data yang akan di proses :");
+        int u = Integer.parseInt(reader.readLine());
+        int counter = 0;
+        for (int index = 0; index < u; index++) {
+            counter++;
+            System.out.print("Masukkan Nama " + counter + ":");
+            String name = reader.readLine();
+            System.out.print("Huruf Yang akan Dihitung ");
+            char s = (char) reader.read();
+            g.addAll(CountWords(name, s));
+        }
+        for (int d : g) {
+            System.out.print(g + " ");
+        }
+    }
+
+    private static ArrayList<Integer> CountWords(String name, char s) {
+        ArrayList<Integer> result = new ArrayList<>();
+        int count = 0;
+
+        for (char c : name.toCharArray()) {
+            if (c == s) {
+                count++;
+            }
+        }
+        result.add(count);
+        return result;
+    }
+}
